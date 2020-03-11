3a38dcec8c7f43fd52143972854689ce61f0d996
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2020-03-10 23:33:25 -0300

    Inicio da alteração de alterar senha

--- central-de-erros/src/view/usuario-recuperar-senha/index.js
+++ central-de-erros/src/view/usuario-recuperar-senha/index.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
-import { toast } from 'react-toastify';
-import firebase from '../../config/firebase';
-import 'firebase/auth';
-import './style.css';
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import firebase from "../../config/firebase";
+import "firebase/auth";
+import { DivCard, DivFundo, Form, Button } from "./style";
 
 function UsuarioRecuperarSenha() {
-
   const [email, setEmail] = useState();
 
   function recuperarSenha() {
-    firebase.auth().sendPasswordResetEmail(email).then(resultado => {
-      toast.success(`Enviamos um link para seu email ${email} para que você redefina sua senha !`);
-    }).catch(erro => {
-      toast.error(`Erro`);
-    });
-  };
+    firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then(resultado => {
+        toast.success(
+          `Enviamos um link para seu email ${email} para que você redefina sua senha !`
+        );
+      })
+      .catch(erro => {
+        toast.error(`Erro`);
+      });
+  }
 
   return (
-    <div className="form-cadastro" >
-      <form >
-        <h3><i class="fa fa-envelope"></i> Recuperar Senha</h3>
-        <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" placeholder="Email" />
+    <DivFundo>
+      <DivCard>
+        <Form>
+          <h2>
+            <i className="fa fa-envelope"></i> Recuperar Senha
+          </h2>
+          <input
+            onChange={e => setEmail(e.target.value)}
+            type="email"
+            className="form-control"
+            placeholder="Email"
+          />
 
-        <button onClick={recuperarSenha} type="button" className="btn-enviar">Recuperar Senha</button>
-      </form>
-    </div>
+          <Button onClick={recuperarSenha} type="button" className="btn-enviar">
+            Recuperar Senha
+          </Button>
+        </Form>
+      </DivCard>
+    </DivFundo>
   );
 }
 
---
+++ central-de-erros/src/view/usuario-recuperar-senha/style.js
@@ -0,0 +1,137 @@
+import styled from "styled-components";
+
+export const DivFundo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const DivCard = styled.div`
+  align-self: center;
+  background-color: #061e2d;
+  border-radius: 10px;
+  width: 40%;
+  padding: 15px;
+  font-size: 1.1rem;
+  @media (min-width: 426px) {
+    padding: 20px;
+    line-height: 1.5rem;
+    h1 {
+      font-size: 1.5rem;
+      padding-bottom: 10px;
+    }
+  }
+  @media (max-width: 425px) {
+    padding: 10px;
+    h1 {
+      font-size: 1.1rem;
+    }
+    width: 96%;
+    border-radius: 3% 3% 0px 0px;
+    font-size: 0.75rem;
+  }
+  @media (max-width: 325px) {
+    padding: 10px;
+    width: 90%;
+    border-radius: 3% 3% 0px 0px;
+  }
+  display: flex;
+  color: #ffffff;
+  flex-direction: column;
+  font-family: "Roboto";
+  /* line-height: 1rem; */
+  strong {
+    color: #cd683e;
+  }
+  hr {
+    width: 100%;
+  }
+
+  h2,
+  h3 {
+    font-size: 0.9rem;
+    color: #dd9d3c;
+  }
+`;
+
+export const AsideComponent = styled.aside`
+  width: fit-content;
+  word-break: break-all;
+  @media (min-width: 426px) {
+    padding: 10px;
+    width: 20%;
+  }
+`;
+
+export const MainComponent = styled.main`
+  width: 100%;
+  @media (min-width: 426px) {
+    padding: 10px;
+    width: 75%;
+  }
+  word-break: break-all;
+`;
+
+export const SectionErro = styled.section`
+  display: flex;
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
+`;
+
+export const Button = styled.button`
+  box-sizing: border-box;
+  background-color: #cd683e;
+  color: #ffffff;
+
+  border: 0;
+  padding: 12px 12px 12px 12px;
+  margin-bottom: 5px;
+  font-size: 20px;
+  transition: background-color 0.2s ease;
+  font-weight: 700;
+
+  @media (max-width: 425px) {
+    order: 1;
+    border-radius: 0px 0px 10px 10px;
+    width: 96%;
+    padding: 10px;
+  }
+  @media (min-width: 426px) {
+    border-radius: 4px;
+    width: 15%;
+    align-self: flex-start;
+    margin-left: 5%;
+  }
+  &:hover {
+    background-color: #c23439;
+    color: #ffffff;
+  }
+`;
+
+export const Level = styled.p`
+  .error,
+  .warning,
+  .debug {
+    display: flex;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+  .error {
+    background-color: #ff0000aa;
+  }
+  .warning {
+    background-color: #ff9900aa;
+  }
+  .debug {
+    background-color: #007700aa;
+  }
+`;
